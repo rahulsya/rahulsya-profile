ff90f34fc2ea76257c3aea5383427eb7b94fa462
rahulsya <rahulsyaban@gmai.com>
2021-03-11 20:14:29 +0700

    add new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily:{
+      'poppins':['Poppins','sans-serif']
+    }
   },
   variants: {
     extend: {},
