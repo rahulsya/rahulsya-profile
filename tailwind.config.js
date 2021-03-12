0d760c62523c779157dcbdaa9db5ce4fa0b0afa5
rahulsya <48597821+rahulsya@users.noreply.github.com>
2021-03-12 22:31:54 +0700

    add color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,23 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
-    fontFamily:{
-      'poppins':['Poppins','sans-serif']
-    }
+    extend: {
+      colors: {
+        gelap: {
+          primary: "#333333",
+          secondary: "#4F4F4F",
+          third: "#828282",
+        },
+        terang: {
+          primary: "#FFFFFF",
+          secondary: "#F2F2F2",
+          third: "#E0E0E0",
+        },
+      },
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
   },
   variants: {
     extend: {},
